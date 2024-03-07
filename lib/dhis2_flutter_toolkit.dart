158b71e4abc94463ed39d40945092d334bb5bc9d
Gift Nnko <nnkogift@hotmail.com>
2024-03-07 08:09:45 +0300

    chore(auth service): Refactor
    - Exported all public services

--- lib/dhis2_flutter_toolkit.dart
+++ lib/dhis2_flutter_toolkit.dart
@@ -1,4 +1,5 @@
 library dhis2_flutter_toolkit;
 
 export "./src/services/auth_service/auth_service.dart" show D2AuthService;
+export "./src/services/auth_service/credentials.dart" show D2UserCredential;
 export "./src/services/client/client.dart" show D2ClientService;
