0504f72b9a648cd40e665b23e412b01ff7d4ba17
Gift Nnko <nnkogift@hotmail.com>
2024-03-07 10:55:10 +0300

    chore(*): Cleanup  - Cleaned up unwanted files

--- lib/src/utils/db.dart
+++
@@ -1,9 +0,0 @@
-import 'package:objectbox/objectbox.dart';
-
-int? getIdByUid<T extends Entity>(
-    {required Box<T> box,
-    required Condition<T> condition,
-    required String uid}) {
-  Query<T> query = box.query(condition).build();
-  return query.findFirst()?.uid;
-}
