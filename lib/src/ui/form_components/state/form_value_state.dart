e75b54ffdf43dbf90f8af610cfc628030e3ce266
Gift Nnko <nnkogift@hotmail.com>
2024-03-22 11:22:30 +0300

    feat(form controller): Improvements
    - Added validation and submit functionalities in form controller

--- lib/src/ui/form_components/state/form_value_state.dart
+++ lib/src/ui/form_components/state/form_value_state.dart
@@ -3,16 +3,20 @@ import 'package:flutter/foundation.dart';
 mixin D2FormValueState on ChangeNotifier {
   final Map<String, dynamic> _formValues = {};
 
-  get formValues {
+  Map<String, dynamic> get formValues {
     return Map.from(_formValues);
   }
 
-  setValue(String key, value) {
+  void setValues(Map<String, dynamic> values) {
+    _formValues.addAll(values);
+  }
+
+  void setValue(String key, value) {
     _formValues[key] = value;
     notifyListeners();
   }
 
-  clearValue(String key) {
+  void clearValue(String key) {
     _formValues.remove(key);
     notifyListeners();
   }
