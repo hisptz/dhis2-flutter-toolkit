32c1d9bef49ba61ceed6d68b9d86328c3dacfa1b
Gift Nnko <nnkogift@hotmail.com>
2024-04-27 00:33:37 +0300

    feat(aggregate-repo): Improvements
    - Improved data value set query

--- lib/src/repositories/data/query_mixin/data_value_set_query_mixin.dart
+++ lib/src/repositories/data/query_mixin/data_value_set_query_mixin.dart
@@ -7,14 +7,28 @@ mixin D2DataValueSetQueryMixin on D2BaseAggregateQueryMixin<D2DataValueSet> {
   setAttributeOptionCombo(D2CategoryOptionCombo attributeCombo) {
     updateQueryCondition(
         D2DataValueSet_.attributeOptionCombo.equals(attributeCombo.id));
+    return this;
   }
 
   setOrgUnit(D2OrgUnit orgUnit) {
     updateQueryCondition(D2DataValueSet_.organisationUnit.equals(orgUnit.id));
+    return this;
   }
 
   setPeriod(String period) {
     updateQueryCondition(D2DataValueSet_.period.equals(period));
+    return this;
+  }
+
+  setDataElement(D2DataElement dataElement) {
+    updateQueryCondition(D2DataValueSet_.dataElement.equals(dataElement.id));
+    return this;
+  }
+
+  setCategoryOptionCombo(D2CategoryOptionCombo categoryOptionCombo) {
+    updateQueryCondition(
+        D2DataValueSet_.categoryOptionCombo.equals(categoryOptionCombo.id));
+    return this;
   }
 
   setDataSet(D2DataSet dataSet) {
@@ -25,5 +39,6 @@ mixin D2DataValueSetQueryMixin on D2BaseAggregateQueryMixin<D2DataValueSet> {
       throw "You cannot set a data set with more than one attribute option. Call setAttributeOptionCombo instead";
     }
     setAttributeOptionCombo(options.first);
+    return this;
   }
 }
