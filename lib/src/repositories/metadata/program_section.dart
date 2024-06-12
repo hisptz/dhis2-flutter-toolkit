05ea9a57fbf91c09155dd015257b9cafb14d64ec
Gift Nnko <nnkogift@hotmail.com>
2024-06-12 17:22:47 +0300

    fix: tracker download
    Fixed issues with sync of program sections & program stage sections causing some issues if fields are changed within a section

--- lib/src/repositories/metadata/program_section.dart
+++ lib/src/repositories/metadata/program_section.dart
@@ -1,6 +1,6 @@
+import 'package:dhis2_flutter_toolkit/dhis2_flutter_toolkit.dart';
 import 'package:dhis2_flutter_toolkit/objectbox.g.dart';
 
-import '../../models/metadata/program_section.dart';
 import 'base.dart';
 
 class D2ProgramSectionRepository extends BaseMetaRepository<D2ProgramSection> {
@@ -16,5 +16,25 @@ class D2ProgramSectionRepository extends BaseMetaRepository<D2ProgramSection> {
   @override
   D2ProgramSection mapper(Map<String, dynamic> json) {
     return D2ProgramSection.fromMap(db, json);
+  }
+
+  @override
+  Future<List<D2ProgramSection>> saveOffline(List<Map<String, dynamic>> json) {
+    //We need to delete existing sections for the program in order to record them again
+    String programUid = json.first["program"]["id"] as String;
+    D2Program? program = D2ProgramRepository(db).getByUid(programUid);
+
+    if (program != null) {
+      List<int> sectionIds = box
+          .query(D2ProgramSection_.program.equals(program.id))
+          .build()
+          .findIds();
+
+      if (sectionIds.isNotEmpty) {
+        box.removeMany(sectionIds);
+      }
+    }
+
+    return super.saveOffline(json);
   }
 }
