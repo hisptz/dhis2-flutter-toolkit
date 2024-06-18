81b6e407b55558fb3b2efbfd61cd9f97e508ddae
Davis799 <davistibz@gmail.com>
2024-06-18 09:32:47 +0300

    docs(d2dataelementrepository): docs

--- lib/src/repositories/metadata/data_element.dart
+++ lib/src/repositories/metadata/data_element.dart
@@ -1,11 +1,17 @@
-
 import '../../../objectbox.g.dart';
 import '../../models/metadata/data_element.dart';
 import 'base.dart';
 
+/// This is a repository class for managing DHIS2 data elements.
 class D2DataElementRepository extends BaseMetaRepository<D2DataElement> {
+  /// Constructs a new [D2DataElementRepository].
+  ///
+  /// - [db]: The ObjectBox database.
   D2DataElementRepository(super.db);
 
+  /// Returns a data element by its [uid].
+  ///
+  /// - [uid]: The UID of the data element to retrieve.
   @override
   D2DataElement? getByUid(String uid) {
     Query<D2DataElement> query =
@@ -13,6 +19,9 @@ class D2DataElementRepository extends BaseMetaRepository<D2DataElement> {
     return query.findFirst();
   }
 
+  /// Maps JSON data to a [D2DataElement] object.
+  ///
+  /// - [json]: The JSON data to map.
   @override
   D2DataElement mapper(Map<String, dynamic> json) {
     return D2DataElement.fromMap(db, json);
