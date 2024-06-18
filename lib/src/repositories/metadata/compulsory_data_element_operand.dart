3e27401e65aa24379c1614e9d5af0e862aac0c56
Davis799 <davistibz@gmail.com>
2024-06-18 09:32:16 +0300

    docs(d2compulsorydataelementoperandrepository): docs

--- lib/src/repositories/metadata/compulsory_data_element_operand.dart
+++ lib/src/repositories/metadata/compulsory_data_element_operand.dart
@@ -3,10 +3,19 @@ import 'package:dhis2_flutter_toolkit/src/models/metadata/compulsory_data_elemen
 
 import 'base.dart';
 
+/// This is a repository class for managing compulsory data element operands in DHIS2.
 class D2CompulsoryDataElementOperandRepository
     extends BaseMetaRepository<D2CompulsoryDataElementOperand> {
+  /// Creates a new instance of [D2CompulsoryDataElementOperandRepository].
+  ///
+  /// - [db]: The ObjectBox database instance.
   D2CompulsoryDataElementOperandRepository(super.db);
 
+  /// Retrieves a compulsory data element operand by its [uid].
+  ///
+  /// Returns a [D2CompulsoryDataElementOperand] instance if found, otherwise returns `null`.
+  ///
+  /// - [uid]: The UID of the compulsory data element operand to retrieve.
   @override
   D2CompulsoryDataElementOperand? getByUid(String uid) {
     Query<D2CompulsoryDataElementOperand> query =
@@ -14,6 +23,11 @@ class D2CompulsoryDataElementOperandRepository
     return query.findFirst();
   }
 
+  /// Maps a JSON representation to a [D2CompulsoryDataElementOperand] instance.
+  ///
+  /// - [json]: The JSON map representing the compulsory data element operand.
+  ///
+  /// Returns a [D2CompulsoryDataElementOperand] instance.
   @override
   D2CompulsoryDataElementOperand mapper(Map<String, dynamic> json) {
     return D2CompulsoryDataElementOperand.fromMap(db, json);
