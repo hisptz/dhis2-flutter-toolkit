555ccaa3f21eed1d44df8ac72ba4d87d94677159
Davis799 <davistibz@gmail.com>
2024-06-18 09:31:02 +0300

    docs(d2categoryoptioncomborepository): docs

--- lib/src/repositories/metadata/category_option_combo.dart
+++ lib/src/repositories/metadata/category_option_combo.dart
@@ -2,10 +2,17 @@ import '../../../objectbox.g.dart';
 import '../../models/metadata/category_option_combo.dart';
 import './base.dart';
 
+/// This is a repository class for managing Category Option Combo instances.
 class D2CategoryOptionComboRepository
     extends BaseMetaRepository<D2CategoryOptionCombo> {
+  /// Creates a new instance of [D2CategoryOptionComboRepository].
+  ///
+  /// - [db]: The ObjectBox database instance.
   D2CategoryOptionComboRepository(super.db);
 
+  /// Retrieves a [D2CategoryOptionCombo] instance by its [uid].
+  ///
+  /// Returns the [D2CategoryOptionCombo] instance with the specified [uid], if found; otherwise, returns `null`.
   @override
   D2CategoryOptionCombo? getByUid(String uid) {
     Query<D2CategoryOptionCombo> query =
@@ -13,6 +20,11 @@ class D2CategoryOptionComboRepository
     return query.findFirst();
   }
 
+  /// Maps JSON data to a [D2CategoryOptionCombo] instance.
+  ///
+  /// - [json]: The JSON data to be mapped.
+  ///
+  /// Returns the mapped [D2CategoryOptionCombo] instance.
   @override
   D2CategoryOptionCombo mapper(Map<String, dynamic> json) {
     return D2CategoryOptionCombo.fromMap(db, json);
