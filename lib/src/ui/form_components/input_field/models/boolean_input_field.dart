37dc306f12ea1dd8daf67a86d6e607c4523f1be6
Davis799 <davistibz@gmail.com>
2024-06-18 09:37:14 +0300

    docs(d2booleaninputfieldconfig): docs

--- lib/src/ui/form_components/input_field/models/boolean_input_field.dart
+++ lib/src/ui/form_components/input_field/models/boolean_input_field.dart
@@ -1,6 +1,17 @@
 import 'base_input_field.dart';
 
+/// Configuration class for a boolean input field.
 class D2BooleanInputFieldConfig extends D2BaseInputFieldConfig {
+  /// Creates a new instance of [D2BooleanInputFieldConfig].
+  ///
+  /// - [label]: The label for the input field.
+  /// - [type]: The type of input field.
+  /// - [name]: The name of the input field.
+  /// - [mandatory]: Whether the input field is mandatory.
+  /// - [clearable]: Whether the input field can be cleared.
+  /// - [icon]: The icon for the input field.
+  /// - [legends]: The legends for the input field.
+  /// - [svgIconAsset]: The SVG icon asset for the input field.
   D2BooleanInputFieldConfig(
       {required super.label,
       required super.type,
