d27aa424d80ad21c3ecf9c8b90cb874a8e7def83
Davis799 <davistibz@gmail.com>
2024-06-19 10:38:55 +0300

    docs(d2enrollment): docs

--- lib/src/models/data/enrollment.dart
+++ lib/src/models/data/enrollment.dart
@@ -17,43 +17,82 @@ import 'tracked_entity.dart';
 import 'upload_base.dart';
 
 @Entity()
+
+/// This class represents an enrollment in the DHIS2 system.
 class D2Enrollment extends SyncDataSource
     implements SyncableData, D2BaseEditable, D2BaseDeletable {
+  /// Unique identifier for the enrollment.
   @override
   int id = 0;
+
+  /// Date and time when the enrollment was created.
   @override
   DateTime createdAt;
 
+  /// Date and time when the enrollment was last updated.
   @override
   DateTime updatedAt;
 
+  /// Unique identifier for the enrollment.
   @override
   @Unique()
   String uid;
+
+  /// Date when the enrollment was started.
   DateTime enrolledAt;
+
+  /// Indicates if the enrollment has been deleted.
   bool deleted;
+
+  /// Indicates if follow-up is required for the enrollment.
   bool followup;
+
+  /// Date when the enrollment event occurred.
   DateTime occurredAt;
+
+  /// Current status of the enrollment.
   String status;
+
+  /// Additional notes related to the enrollment.
   String? notes;
+
+  /// JSON string representation of the geometry associated with the enrollment.
   String? geometry;
 
+  /// Backlink to events associated with this enrollment.
   @Backlink("enrollment")
   final events = ToMany<D2Event>();
 
+  /// Backlink to relationships associated with this enrollment.
   @Backlink("fromEnrollment")
   final relationships = ToMany<D2Relationship>();
 
+  /// To-one relation to the tracked entity associated with this enrollment.
   final trackedEntity = ToOne<D2TrackedEntity>();
 
+  /// To-one relation to the organization unit associated with this enrollment.
   final orgUnit = ToOne<D2OrgUnit>();
 
+  /// To-one relation to the program associated with this enrollment.
   final program = ToOne<D2Program>();
 
   get orgUnitName {
     return orgUnit.target?.name;
   }
 
+  /// Constructor for creating an instance of [D2Enrollment].
+  /// Parameters:
+  /// - [uid]: Unique identifier for the enrollment.
+  /// - [updatedAt]: Date and time when the enrollment was last updated.
+  /// - [createdAt]: Date and time when the enrollment was created.
+  /// - [enrolledAt]: Date when the enrollment was started.
+  /// - [followup]: Whether follow-up is required for the enrollment.
+  /// - [deleted]: Whether the enrollment has been deleted.
+  /// - [occurredAt]: Date when the enrollment event occurred.
+  /// - [status]: Current status of the enrollment.
+  /// - [notes]: Additional notes related to the enrollment.
+  /// - [synced]: Whether the enrollment has been synced with the server.
+  /// - [geometry]: JSON string representation of the geometry associated with the enrollment.
   D2Enrollment(
       this.uid,
       this.updatedAt,
@@ -67,6 +106,7 @@ class D2Enrollment extends SyncDataSource
       this.synced,
       this.geometry);
 
+  /// Constructor that creates an instance of [D2Enrollment] from a JSON [json].
   D2Enrollment.fromMap(D2ObjectBox db, Map json)
       : uid = json["enrollment"],
         updatedAt = DateTime.parse(json["updatedAt"]),
@@ -88,6 +128,7 @@ class D2Enrollment extends SyncDataSource
     program.target = D2ProgramRepository(db).getByUid(json["program"]);
   }
 
+  /// Constructor that creates an instance of [D2Enrollment] from form values [values].
   D2Enrollment.fromFormValues(Map<String, dynamic> values,
       {required D2ObjectBox db,
       required D2TrackedEntity trackedEntity,
@@ -112,7 +153,7 @@ class D2Enrollment extends SyncDataSource
     if (values["geometry"] != null) {
       var geometryValue = values["geometry"];
 
-      ///A form has geometry. This should be inserted as a serialized JSON
+      /// A form has geometry. This should be inserted as a serialized JSON.
       if (geometryValue is D2GeometryValue) {
         Map<String, dynamic> geometry = geometryValue.toGeoJson();
         String geometryString = jsonEncode(geometry);
@@ -121,9 +162,11 @@ class D2Enrollment extends SyncDataSource
     }
   }
 
+  /// Whether the enrollment has been synced with the server.
   @override
   bool synced;
 
+  /// Converts the enrollment object to a JSON map for serialization.
   @override
   Future<Map<String, dynamic>> toMap({D2ObjectBox? db}) async {
     if (db == null) {
@@ -149,6 +192,9 @@ class D2Enrollment extends SyncDataSource
     return payload;
   }
 
+  /// Converts the enrollment object to form values.
+  ///
+  /// Returns a [Map<String, dynamic>]
   @override
   Map<String, dynamic> toFormValues() {
     Map<String, dynamic> data = {
@@ -165,6 +211,7 @@ class D2Enrollment extends SyncDataSource
     return data;
   }
 
+  /// Updates the enrollment object from form input values.
   @override
   void updateFromFormValues(Map<String, dynamic> values,
       {required D2ObjectBox db, D2Program? program, D2OrgUnit? orgUnit}) {
@@ -179,7 +226,7 @@ class D2Enrollment extends SyncDataSource
     if (values["geometry"] != null) {
       var geometryValue = values["geometry"];
 
-      ///A form has geometry. This should be inserted as a serialized JSON
+      /// A form has geometry. This should be inserted as a serialized JSON.
       if (geometryValue is D2GeometryValue) {
         Map<String, dynamic> geometry = geometryValue.toGeoJson();
         String geometryString = jsonEncode(geometry);
@@ -190,11 +237,15 @@ class D2Enrollment extends SyncDataSource
     synced = false;
   }
 
+  /// Saves the enrollment object to the database.
   @override
   void save(D2ObjectBox db) {
     id = D2EnrollmentRepository(db).saveEntity(this);
   }
 
+  /// Deletes the enrollment object from the database.
+  ///
+  /// Returns a [bool] whether the enrollment object was deleted
   @override
   bool delete(D2ObjectBox db) {
     for (D2Event event in events) {
@@ -206,6 +257,7 @@ class D2Enrollment extends SyncDataSource
     return D2EnrollmentRepository(db).deleteEntity(this);
   }
 
+  /// Soft deletes the enrollment by marking it as deleted in the database.
   @override
   void softDelete(db) {
     deleted = true;
