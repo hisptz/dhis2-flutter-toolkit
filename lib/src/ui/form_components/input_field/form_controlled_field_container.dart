178d3f701d18d2c84c488663aee68102fe1fa30c
Davis799 <davistibz@gmail.com>
2024-06-19 10:58:11 +0300

    docs(d2formcontrolledinputfield): docs

--- lib/src/ui/form_components/input_field/form_controlled_field_container.dart
+++ lib/src/ui/form_components/input_field/form_controlled_field_container.dart
@@ -1,48 +1,65 @@
 import 'package:dhis2_flutter_toolkit/dhis2_flutter_toolkit.dart';
 import 'package:flutter/material.dart';
-
 import '../state/field_state.dart';
 
+/// A controlled input field widget that integrates with a dynamic form controller.
 class D2FormControlledInputField extends StatelessWidget {
+  /// The configuration for the input field.
   final D2BaseInputFieldConfig input;
+
+  /// The color theme for the input field.
   final Color? color;
+
+  /// The form controller managing the state of the form.
   final D2FormController controller;
+
+  /// Whether the input field is disabled.
   final bool? disabled;
+
+  /// Custom input decoration for the field.
   final D2InputDecoration? inputDecoration;
 
-  const D2FormControlledInputField(
-      {super.key,
-      required this.input,
-      this.disabled,
-      this.inputDecoration,
-      required this.controller,
-      required this.color});
+  /// Constructs a [D2FormControlledInputField] instance.
+  /// - [input]: The configuration for the input field.
+  /// - [color]: The color theme for the input field.
+  /// - [controller]: The form controller managing the state of the form.
+  /// - [disabled]: Whether the input field is disabled.
+  /// - [inputDecoration]: Custom input decoration for the field.
+  const D2FormControlledInputField({
+    Key? key,
+    required this.input,
+    this.disabled,
+    this.inputDecoration,
+    required this.controller,
+    required this.color,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListenableBuilder(
-        listenable: controller,
-        builder: (BuildContext context, Widget? child) {
-          FieldState fieldState = controller.getFieldState(input.name);
+      listenable: controller,
+      builder: (BuildContext context, Widget? child) {
+        FieldState fieldState = controller.getFieldState(input.name);
 
-          if (input is D2SelectInputFieldConfig) {
-            (input as D2SelectInputFieldConfig).optionsToHide =
-                fieldState.optionsToHide;
-          }
+        if (input is D2SelectInputFieldConfig) {
+          (input as D2SelectInputFieldConfig).optionsToHide =
+              fieldState.optionsToHide;
+        }
 
-          return Visibility(
-            visible: !(fieldState.hidden ?? false),
-            child: D2InputFieldContainer(
-              inputDecoration: inputDecoration,
-              input: input,
-              onChange: fieldState.onChange,
-              color: color,
-              error: fieldState.error,
-              warning: fieldState.warning,
-              value: fieldState.value,
-              disabled: (fieldState.disabled ?? false) || (disabled ?? false),
-            ),
-          );
-        });
+        return Visibility(
+          visible: !(fieldState.hidden ?? false),
+          child: D2InputFieldContainer(
+            inputDecoration: inputDecoration,
+            input: input,
+            onChange: fieldState.onChange,
+            color: color,
+            error: fieldState.error,
+            warning: fieldState.warning,
+            value: fieldState.value,
+            disabled: (fieldState.disabled ?? false) || (disabled ?? false),
+          ),
+        );
+      },
+    );
   }
 }
