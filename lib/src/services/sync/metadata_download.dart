d878b0363c06bcf7310e594aec021a7a7240b6e1
Davis799 <davistibz@gmail.com>
2024-06-20 10:13:31 +0300

    docs(d2metadatadownloadservice): docs

--- lib/src/services/sync/metadata_download.dart
+++ lib/src/services/sync/metadata_download.dart
@@ -2,17 +2,24 @@ import 'dart:async';
 
 import 'package:dhis2_flutter_toolkit/dhis2_flutter_toolkit.dart';
 
+/// This is a service class for downloading and syncing metadata in a DHIS2 system.
 class D2MetadataDownloadService {
   D2ObjectBox db;
   D2ClientService client;
   StreamController<D2SyncStatus> controller = StreamController<D2SyncStatus>();
 
+  /// Returns the stream of sync status updates.
   get stream {
     return controller.stream;
   }
 
+  /// Constructs a [D2MetadataDownloadService] with the provided database and client service.
   D2MetadataDownloadService(this.db, this.client);
 
+  /// Sets up the metadata download for the specified [user] and streams the download status.
+  ///
+  /// Parameters:
+  /// - [user]: The [D2User] whose metadata will be downloaded.
   Future setupUserMetadataDownload(D2User user) async {
     D2ProgramRepository programRepository = D2ProgramRepository(db);
     List<String> programs = user.programs;
@@ -24,6 +31,7 @@ class D2MetadataDownloadService {
     await controller.addStream(dataSetRepository.downloadStream);
   }
 
+  /// Sets up the metadata download for the system and streams the download status.
   Future setupMetadataDownload() async {
     D2UserRepository userRepository = D2UserRepository(db);
     userRepository.setupDownload(client).download();
@@ -62,6 +70,7 @@ class D2MetadataDownloadService {
     await setupUserMetadataDownload(user);
   }
 
+  /// Sets up the metadata sync process and marks it as complete.
   Future setupSync() async {
     await setupMetadataDownload();
     controller
@@ -69,6 +78,7 @@ class D2MetadataDownloadService {
     controller.close();
   }
 
+  /// Initiates the metadata download and sync process.
   Future<void> download() async {
     await setupSync();
   }
