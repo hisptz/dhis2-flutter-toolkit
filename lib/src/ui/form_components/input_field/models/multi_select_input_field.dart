0420a0be14b2935cfbd11aa9ce2e320c6ceda892
Davis799 <davistibz@gmail.com>
2024-06-20 10:21:49 +0300

    docs(d2multiselectinputfieldconfig): docs

--- lib/src/ui/form_components/input_field/models/multi_select_input_field.dart
+++ lib/src/ui/form_components/input_field/models/multi_select_input_field.dart
@@ -1,9 +1,23 @@
 import 'package:dhis2_flutter_toolkit/dhis2_flutter_toolkit.dart';
 
+/// Configuration for a multi-select input field, extending [D2SelectInputFieldConfig].
 class D2MultiSelectInputFieldConfig extends D2SelectInputFieldConfig {
+  /// Maximum number of selections allowed.
   int? maxSelections;
+
+  /// Whether the options should be displayed horizontally.
   bool horizontal = false;
 
+  /// Constructs a [D2MultiSelectInputFieldConfig].
+  ///
+  /// Parameters:
+  /// - [options]: Options available for selection.
+  /// - [maxSelections]: Maximum number of selections allowed.
+  /// - [horizontal]: Whether to display options horizontally.
+  /// - [label]: Label for the input field.
+  /// - [type]: Type of the input field.
+  /// - [name]: Name of the input field.
+  /// - [mandatory]: Whether the field is mandatory.
   D2MultiSelectInputFieldConfig(
       {required super.options,
       this.maxSelections,
