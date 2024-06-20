670002e89dcf9ee415625ed48385705d41a419bf
Davis799 <davistibz@gmail.com>
2024-06-20 10:03:19 +0300

    docs(d2objectbox): docs

--- lib/objectbox.dart
+++ lib/objectbox.dart
@@ -4,15 +4,22 @@ import 'package:path_provider/path_provider.dart';
 import 'objectbox.g.dart';
 import 'src/services/entry.dart'; // created by `flutter pub run build_runner build`
 
+/// Manages the ObjectBox store and provides methods to create instances.
 class D2ObjectBox {
-  /// The Store of this app.
+  /// The store instance for the app.
   final Store store;
+
+  /// Identifier for the store.
   final String storeId;
 
+  /// Private constructor for creating a [D2ObjectBox] instance.
   D2ObjectBox._create(this.store, this.storeId) {
     // Add any additional setup code, e.g. build queries.
   }
 
+  /// Creates a test instance of [D2ObjectBox] with an in-memory store.
+  ///
+  /// Returns a [Future] that completes with the created [D2ObjectBox] instance.
   static Future<D2ObjectBox> createTest() async {
     final store = await openStore(
       directory: "memory:test-db",
@@ -20,7 +27,10 @@ class D2ObjectBox {
     return D2ObjectBox._create(store, "test");
   }
 
-  /// Create an instance of ObjectBox to use throughout the app.
+  /// Creates an instance of [D2ObjectBox] for use throughout the app.
+  ///
+  /// Takes [credentials] of type [D2UserCredential] to identify the store.
+  /// Returns a [Future] that completes with the created [D2ObjectBox] instance.
   static Future<D2ObjectBox> create(D2UserCredential credentials) async {
     String storeId = credentials.id;
     final docsDir = await getApplicationDocumentsDirectory();
