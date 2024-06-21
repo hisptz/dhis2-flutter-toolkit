eaec58c87706fb4cd147dab16cb9157141d08827
Davis799 <davistibz@gmail.com>
2024-06-21 09:59:31 +0300

    docs(d2periodselection): docs

--- lib/src/utils/period_engine/models/period_filter_selection.dart
+++ lib/src/utils/period_engine/models/period_filter_selection.dart
@@ -8,33 +8,58 @@ import 'package:dhis2_flutter_toolkit/src/utils/period_engine/constants/period_c
 import 'period.dart';
 import 'period_type.dart';
 
+/// This class represents a period selection with various attributes and utility methods.
 class D2PeriodSelection {
+  /// The category of the period selection.
   late String category;
+
+  /// The type of the period selection.
   String? type;
+
+  /// List of selected period IDs.
   List<String>? selected = [];
+
+  /// Start date of the period.
   DateTime? start;
+
+  /// End date of the period.
   DateTime? end;
 
+  /// Display name for the period selection.
   String? displayName;
 
-  // Constructor for PeriodSelection class
+  /// Constructs a [D2PeriodSelection].
+  ///
+  /// - [category]: The category of the period selection.
+  /// - [type]: The type of the period selection.
+  /// - [selected]: List of selected period IDs.
+  /// - [start]: Start date of the period.
+  /// - [end]: End date of the period.
   D2PeriodSelection(
       {required this.category, this.type, this.selected, this.start, this.end});
 
-  // Override toString method to customize string representation of the object
+  /// Override toString method to customize string representation of the object
+  /// Returns the string representation of the object.
   @override
   String toString() {
     return "$category ${type ?? ''}";
   }
 
-  // Utility method to convert camelCase to snake_case
+  /// Converts camelCase to snake_case.
+  ///
+  /// - [value]: The camelCase string.
+  /// - [separator]: The separator to use (default is '_').
+  ///
+  /// Returns the snake_case version of the input string.
   String uncamelize(String value, {String separator = '_'}) {
     return value.replaceAllMapped(RegExp(r'[A-Z]|\d+'), (match) {
       return '$separator${match[0]}';
     }).toUpperCase();
   }
 
-  // Constructor to create PeriodSelection object from a list of selection IDs
+  /// Creates a [D2PeriodSelection] object from a list of selection IDs.
+  ///
+  /// - [selection]: List of period IDs.
   D2PeriodSelection.fromSelection(List<String> selection) {
     if (selection.isNotEmpty) {
       D2Period period = D2PeriodType.getPeriodById(selection.first);
@@ -47,7 +72,9 @@ class D2PeriodSelection {
     }
   }
 
-  // Constructor to create PeriodSelection object from JSON
+  /// Creates a [D2PeriodSelection] object from JSON.
+  ///
+  /// - [json]: The map containing the period selection data.
   D2PeriodSelection.fromJSON(Map json) {
     selected = (json['periods'].cast<Map>())
         ?.map<String>((period) => period['id'] as String)
@@ -67,20 +94,23 @@ class D2PeriodSelection {
     }
   }
 
-  // Getter method to check if the category is range
+  /// Whether the category is range mode.
   get rangeMode => category == D2PeriodTypeCategory.range;
 
+  /// Returns the selected period IDs.
   getSelectionId() {
     return selected;
   }
 
-  // Method to get the display name
+  /// Returns the display name for the period selection.
   String getDisplayName() {
     if (category == '') {}
     return 'Period header';
   }
 
-  // Clone constructor to clone a PeriodSelection object
+  /// Clone constructor to create a copy of a [D2PeriodSelection] object.
+  ///
+  /// - [selection]: The [D2PeriodSelection] object to clone.
   D2PeriodSelection.clone(D2PeriodSelection selection) {
     category = selection.category;
     selected = [...?selection.selected];
