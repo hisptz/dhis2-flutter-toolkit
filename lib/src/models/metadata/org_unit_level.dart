053ed06ba7404148a1d3306c2e4c7087c16b5dc6
Davis799 <davistibz@gmail.com>
2024-06-21 09:51:44 +0300

    docs(d2orgunitlevel): docs

--- lib/src/models/metadata/org_unit_level.dart
+++ lib/src/models/metadata/org_unit_level.dart
@@ -5,25 +5,48 @@ import '../../repositories/metadata/org_unit_level.dart';
 import 'base.dart';
 
 @Entity()
+
+/// This class represents an organizational unit level in the DHIS2 system.
 class D2OrgUnitLevel implements D2MetaResource {
+  /// The unique identifier of the organizational unit level.
   @override
   int id = 0;
 
+  /// The name of the organizational unit level.
   String name;
+
   @override
   @Unique()
   String uid;
+
+  /// The level of the organizational unit.
   int level;
 
+  /// The date and time when the organizational unit level was created.
   @override
   DateTime created;
 
+  /// The date and time when the organizational unit level was last updated.
   @override
   DateTime lastUpdated;
 
-  D2OrgUnitLevel(this.id, this.displayName, this.name, this.uid,
-      this.level, this.created, this.lastUpdated);
+  /// Constructs a [D2OrgUnitLevel].
+  ///
+  /// Parameters:
+  /// - [id]: The unique identifier of the organizational unit level.
+  /// - [displayName]: The display name of the organizational unit level.
+  /// - [name]: The name of the organizational unit level.
+  /// - [uid]: The unique UID of the organizational unit level.
+  /// - [level]: The level of the organizational unit.
+  /// - [created]: The date and time when the organizational unit level was created.
+  /// - [lastUpdated]: The date and time when the organizational unit level was last updated.
+  D2OrgUnitLevel(this.id, this.displayName, this.name, this.uid, this.level,
+      this.created, this.lastUpdated);
 
+  /// Constructs a [D2OrgUnitLevel] from a map [json].
+  ///
+  /// - [db]: The database instance.
+  /// - [json]: The map containing the organizational unit level data.
   D2OrgUnitLevel.fromMap(D2ObjectBox db, Map json)
       : name = json["name"],
         uid = json["id"],
@@ -34,6 +57,7 @@ class D2OrgUnitLevel implements D2MetaResource {
     id = D2OrgUnitLevelRepository(db).getIdByUid(json["id"]) ?? 0;
   }
 
+  /// The display name of the organizational unit level.
   @override
   String? displayName;
 }
