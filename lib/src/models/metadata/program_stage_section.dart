c18a21f6bb85fd4ef0add744c1716c0af07b56b0
Davis799 <davistibz@gmail.com>
2024-06-21 12:21:13 +0300

    docs(d2programstagesection): docs

--- lib/src/models/metadata/program_stage_section.dart
+++ lib/src/models/metadata/program_stage_section.dart
@@ -11,27 +11,51 @@ import 'data_element.dart';
 import 'program_stage.dart';
 
 @Entity()
+
+/// This class represents a section within a program stage of the DHIS2 system.
 class D2ProgramStageSection extends D2MetaResource {
   @override
   int id = 0;
+
+  /// The date and time when the program stage section was created.
   DateTime created;
 
+  /// The date and time when the program stage section was last updated.
   DateTime lastUpdated;
 
+  /// The unique identifier of the program stage section.
   @override
   @Unique()
   String uid;
+
+  /// The name of the program stage section.
   String name;
+
+  /// The sort order of the program stage section.
   int sortOrder;
 
+  /// The associated program stage.
   final programStage = ToOne<D2ProgramStage>();
 
+  /// The data elements associated with this program stage section.
   @Backlink("programStageSection")
-  final programStageSectionDataElements = ToMany<D2ProgramStageSectionDataElement>();
+  final programStageSectionDataElements =
+      ToMany<D2ProgramStageSectionDataElement>();
 
+  /// Creates a new instance of [D2ProgramStageSection].
+  ///
+  /// - [created] is the date and time when the section was created.
+  /// - [lastUpdated] is the date and time when the section was last updated.
+  /// - [uid] is the unique identifier of the section.
+  /// - [name] is the name of the section.
+  /// - [sortOrder] is the sort order of the section.
   D2ProgramStageSection(
       this.created, this.lastUpdated, this.uid, this.name, this.sortOrder);
 
+  /// Creates a new instance of [D2ProgramStageSection] from a JSON object [json].
+  ///
+  /// - [db] is the database instance.
+  /// - [json] is the map containing the data to initialize the instance.
   D2ProgramStageSection.fromMap(D2ObjectBox db, Map json)
       : created = DateTime.parse(json["created"]),
         lastUpdated = DateTime.parse(json["lastUpdated"]),
@@ -61,5 +85,6 @@ class D2ProgramStageSection extends D2MetaResource {
         D2ProgramStageRepository(db).getByUid(json["programStage"]["id"]);
   }
 
+  /// The display name of the program stage section.
   String? displayName;
 }
