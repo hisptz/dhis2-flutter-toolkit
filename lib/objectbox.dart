a7b9461e4d2b70b4159074aa34003452fdcbeba4
Davis799 <davistibz@gmail.com>
2024-06-25 15:24:19 +0300

    docs(d2objectbox): docs

--- lib/objectbox.dart
+++ lib/objectbox.dart
@@ -4,15 +4,17 @@ import 'package:path_provider/path_provider.dart';
 import 'objectbox.g.dart';
 import 'src/services/entry.dart'; // created by `flutter pub run build_runner build`
 
-/// Manages the ObjectBox store and provides methods to create instances.
+/// This class represents a Objectbox store instance used throughout the application.
 class D2ObjectBox {
-  /// The store instance for the app.
+  /// The underlying store instance.
   final Store store;
 
-  /// Identifier for the store.
+  /// Identifier for the store instance.
   final String storeId;
 
   /// Private constructor for creating a [D2ObjectBox] instance.
+  ///
+  /// Use [createTest] or [create] static methods to instantiate [D2ObjectBox].
   D2ObjectBox._create(this.store, this.storeId) {
     // Add any additional setup code, e.g. build queries.
   }
@@ -27,9 +29,10 @@ class D2ObjectBox {
     return D2ObjectBox._create(store, "test");
   }
 
-  /// Creates an instance of [D2ObjectBox] for use throughout the app.
+  /// Creates an instance of [D2ObjectBox] for use throughout the application.
   ///
-  /// Takes [credentials] of type [D2UserCredential] to identify the store.
+  /// - Takes [credentials] of type [D2UserCredential] to identify the store.
+  ///
   /// Returns a [Future] that completes with the created [D2ObjectBox] instance.
   static Future<D2ObjectBox> create(D2UserCredential credentials) async {
     String storeId = credentials.id;
