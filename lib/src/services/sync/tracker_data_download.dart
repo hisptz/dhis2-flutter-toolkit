e4a422892ec4d362158fb01a374cc09e6cbe60e4
Davis799 <davistibz@gmail.com>
2024-06-25 15:35:37 +0300

    docs(d2trackerdatadownloadservice): docs

--- lib/src/services/sync/tracker_data_download.dart
+++ lib/src/services/sync/tracker_data_download.dart
@@ -10,18 +10,36 @@ import '../../repositories/metadata/user.dart';
 import '../../utils/sync_status.dart';
 import '../client/client.dart';
 
+/// This is a service class for managing tracker data downloads.
+///
+/// This class provides methods for setting up and executing tracker data downloads
+/// from the server to the local database.
 class D2TrackerDataDownloadService {
+  /// The database instance.
   D2ObjectBox db;
+
+  /// The client service for making network requests.
   D2ClientService client;
+
+  /// The stream controller for managing download status updates.
   StreamController<D2SyncStatus> downloadController =
       StreamController<D2SyncStatus>();
 
+  /// Constructs a new [D2TrackerDataDownloadService].
+  ///
+  /// - [db] The database instance.
+  /// - [client] The client service for making network requests.
   D2TrackerDataDownloadService(this.db, this.client);
 
+  /// Gets the download status stream.
   get downloadStream {
     return downloadController.stream;
   }
 
+  /// Sets up and starts the data download process.
+  ///
+  /// This method retrieves the user's programs and initiates the download
+  /// for each program's tracked entities, enrollments, and events.
   Future setupDataDownload() async {
     D2User? user = D2UserRepository(db).get();
     if (user == null) {
@@ -56,6 +74,7 @@ class D2TrackerDataDownloadService {
     }
   }
 
+  /// Initiates the download process and marks it as complete upon success.
   Future<void> download() async {
     await setupDataDownload();
     downloadController
