5bc17683392c977ff6cb54afbf07ab749dab274c
Davis799 <davistibz@gmail.com>
2024-06-26 14:50:04 +0300

    docs(d2inputfieldtype): docs

--- lib/src/ui/form_components/input_field/models/input_field_type_enum.dart
+++ lib/src/ui/form_components/input_field/models/input_field_type_enum.dart
@@ -1,38 +1,99 @@
 import 'package:collection/collection.dart';
 
+/// Enumeration representing different types of input fields.
 enum D2InputFieldType {
+  /// Single-line text input.
   text, //TODO:
+
+  /// Multi-line text input.
   longText, //TODO:
+
+  /// Single letter input.
   letter,
+
+  /// Phone number input.
   phoneNumber,
+
+  /// Email address input.
   email,
+
+  /// True/false toggle input.
   trueOnly, //TODO:
+
+  /// Boolean input.
   boolean, //TODO;
+
+  /// Date input.
   date, //TODO:
+
+  /// Date range input.
   dateRange, //TODO:
+
+  /// Date and time input.
   dateTime,
+
+  /// Time input.
   time,
+
+  /// Password input.
   password,
+
+  /// Number input.
   number, //TODO:
+
+  /// Positive integer input.
   positiveInteger,
+
+  /// Unit interval input.
   unitInterval,
+
+  /// Percentage input.
   percentage,
+
+  /// Integer input.
   integer,
+
+  /// Negative integer input.
   negativeInteger,
+
+  /// Non-negative integer input.
   integerZeroOrPositive, //TODO:
+
+  /// Coordinate input.
   coordinate,
+
+  /// Organisation unit input.
   organisationUnit, //TODO:
+
+  /// Reference input.
   reference,
+
+  /// Age input.
   age, //TODO:
+
+  /// URL input.
   url,
+
+  /// File input.
   file,
+
+  /// Image input.
   image,
+
+  /// GeoJSON input.
   geoJson;
 
+  /// Returns the corresponding [D2InputFieldType] for the given [name].
+  ///
+  /// If no match is found, returns `null`.
   static D2InputFieldType? fromName(String? name) {
     return values.firstWhereOrNull((e) => e.name == name);
   }
 
+  /// Returns the corresponding [D2InputFieldType] for the given DHIS2 [valueType].
+  ///
+  /// Converts the [valueType] to a camelCase format and matches it with the enum values.
+  /// If no match is found, returns `null`.
   static D2InputFieldType? fromDHIS2ValueType(String valueType) {
     String sanitizedValueType = valueType
         .toLowerCase()
@@ -46,10 +107,17 @@ enum D2InputFieldType {
         .firstWhereOrNull((element) => element.name == sanitizedValueType);
   }
 
+  /// Checks if the given [type] is a date type.
+  ///
+  /// Returns [bool], Whether the type is [date] or [dateTime].
   static bool isDateType(D2InputFieldType type) {
     return [D2InputFieldType.date, D2InputFieldType.dateTime].contains(type);
   }
 
+  /// Checks if the given [type] is a number type.
+  ///
+  /// Returns `true` if the type is [number], [positiveInteger], [negativeInteger],
+  /// [integer], [integerZeroOrPositive], [unitInterval], or [percentage], otherwise `false`.
   static bool isNumber(D2InputFieldType type) {
     return [
       D2InputFieldType.number,
@@ -62,10 +130,16 @@ enum D2InputFieldType {
     ].contains(type);
   }
 
+  /// Checks if the given [type] is a date range type.
+  ///
+  /// Returns [bool], Whether the type is [dateRange].
   static bool isDateRange(D2InputFieldType type) {
     return [D2InputFieldType.dateRange].contains(type);
   }
 
+  /// Checks if the given [type] is a text type.
+  ///
+  /// Returns [bool], Whether the type is [text], [longText], or [letter].
   static bool isText(D2InputFieldType type) {
     return [
       D2InputFieldType.text,
