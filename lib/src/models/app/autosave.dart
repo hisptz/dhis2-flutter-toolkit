26f5077d7921625489129ed2da5de91ee9c7382e
Davis799 <davistibz@gmail.com>
2024-07-08 15:46:31 +0300

    fix(models): fixes
    - Implemented enrollment and event links in the autosave entity model

--- lib/src/models/app/autosave.dart
+++ lib/src/models/app/autosave.dart
@@ -12,19 +12,40 @@ class D2AppAutoSave extends D2DataResource {
 
   final programStage = ToOne<D2ProgramStage>();
 
+  final enrollment = ToOne<D2Enrollment>();
+
+  final event = ToOne<D2Event>();
+
   D2AppAutoSave(this.id, this.data);
 
   D2AppAutoSave.fromMap(D2ObjectBox db, Map json) : data = json["data"] {
-    id = D2AppAutoSaveRepository(db).getIdByProgram(json["program"]) ?? 0;
-
     if (json["program"] != null) {
       program.target =
           D2ProgramRepository(db).getByUid(json["program"].uid ?? "");
+
+      if (json["enrollment"] != null) {
+        id =
+            D2AppAutoSaveRepository(db).getIdByEnrollment(json["enrollment"]) ??
+                0;
+        enrollment.target =
+            D2EnrollmentRepository(db).getByUid(json["enrollment"].uid ?? "");
+      } else {
+        id = D2AppAutoSaveRepository(db).getIdByProgram(json["program"]) ?? 0;
+      }
     }
 
     if (json["programStage"] != null) {
       programStage.target =
           D2ProgramStageRepository(db).getByUid(json["programStage"].uid ?? "");
+
+      if (json["event"] != null) {
+        id = D2AppAutoSaveRepository(db).getIdByEvent(json["event"]) ?? 0;
+        event.target = D2EventRepository(db).getByUid(json["event"].uid ?? "");
+      } else {
+        id = D2AppAutoSaveRepository(db)
+                .getIdByProgramStage(json["programStage"]) ??
+            0;
+      }
     }
   }
 
