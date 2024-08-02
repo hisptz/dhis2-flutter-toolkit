d5b242714684a67d1ab1e8e084a274aebbe0fdbe
ericchingalo <echingalo@gmail.com>
2024-08-02 12:54:08 +0200

    refactor: made the autosave component re-usable

--- lib/src/ui/form_components/form/dhis2_registration_form.dart
+++ lib/src/ui/form_components/form/dhis2_registration_form.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:dhis2_flutter_toolkit/dhis2_flutter_toolkit.dart';
+import 'package:dhis2_flutter_toolkit/src/ui/form_components/form/auto_save_message_container.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
 
 class D2TrackerRegistrationForm extends StatefulWidget {
   final D2TrackerEnrollmentFormController controller;
@@ -167,76 +168,11 @@ class _D2TrackerRegistrationFormState extends State<D2TrackerRegistrationForm> {
     if (!widget.disableAutoSave &&
         hasAutoSavedValue &&
         widget.onCheckAutoSavedValue != null) {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-              child: Text(
-            widget.autoSaveMessage ??
-                'Would you like to continue with the auto-saved data available?',
-            textAlign: TextAlign.center,
-          )),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: FilledButton(
-                    style: const ButtonStyle().copyWith(
-                      backgroundColor:
-                          WidgetStateProperty.all(Colors.transparent),
-                      foregroundColor: WidgetStatePropertyAll(Colors.grey[600]),
-                      textStyle: WidgetStateProperty.all(
-                        const TextStyle().copyWith(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    onPressed: onDeleteAutoSavedData,
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: 30.0,
-                      child: Text(
-                        'Skip',
-                        style: const TextStyle().copyWith(),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: FilledButton(
-                    style: const ButtonStyle().copyWith(
-                      backgroundColor: WidgetStateProperty.all(formColor),
-                      foregroundColor:
-                          const WidgetStatePropertyAll(Colors.white),
-                      textStyle: WidgetStateProperty.all(
-                        const TextStyle().copyWith(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    onPressed: onContinue,
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: 60.0,
-                      child: Text(
-                        'Continue',
-                        style: const TextStyle().copyWith(),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+      return AutoSaveMessageContainer(
+        formColor: formColor,
+        autoSaveMessage: widget.autoSaveMessage,
+        onDeleteAutoSavedData: onDeleteAutoSavedData,
+        onContinue: onContinue,
       );
     }
 
