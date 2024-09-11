3c5d1f1b07c2c3d9d44d49d619568de113a4b595
Allen7702 <allen22052000@gmail.com>
2024-09-11 16:48:45 +0300

    feat: add feature to get all app logs

--- lib/src/repositories/app/log.dart
+++ lib/src/repositories/app/log.dart
@@ -1,11 +1,9 @@
-import 'package:dhis2_flutter_toolkit/src/models/app/logs.dart';
+import 'package:dhis2_flutter_toolkit/dhis2_flutter_toolkit.dart';
 
-import '../../../objectbox.dart';
 import '../../../objectbox.g.dart';
 
 class D2AppLogRepository {
   D2ObjectBox db;
-
   D2AppLogRepository(this.db);
 
   Box<D2AppLog> get box {
@@ -14,5 +12,10 @@ class D2AppLogRepository {
 
   List<D2AppLog> getLogsByProcess(String process) {
     return box.query(D2AppLog_.process.equals(process)).build().find();
+  }
+
+  List<Map> getAllLogsAsMap() {
+    List<D2AppLog> logs = box.getAll();
+    return logs.map((log) => log.toMap()).toList();
   }
 }
