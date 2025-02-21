a16bee14fda89af66d3cf128e727b36aea2471f0
tibendadavis <davistibz@gmail.com>
2025-02-21 15:50:07 +0300

    feat(bar_code_scanner): enhance barcode scanner input with tap-to-scan option and permission handling

--- lib/src/ui/form_components/input_field/components/bar_code_scanner_input.dart
+++ lib/src/ui/form_components/input_field/components/bar_code_scanner_input.dart
@@ -1,50 +1,122 @@
 import 'package:dhis2_flutter_toolkit/dhis2_flutter_toolkit.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:barcode_scan2/barcode_scan2.dart';
+import 'package:flutter/services.dart';
 import 'base_input.dart';
 import 'input_field_icon.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class BarCodeScannerInput
-    extends BaseStatelessInput<D2BaseInputFieldConfig, String> {
-  BarCodeScannerInput(
+    extends BaseStatefulInput<D2BaseInputFieldConfig, String> {
+  const BarCodeScannerInput(
       {super.key,
       super.disabled,
       required super.input,
       required super.onChange,
       required super.color,
+      this.onScan,
       super.value,
+      this.isTapToScanEnabled = false,
+      this.inputFormatters = const [],
       required super.decoration});
 
-  late TextEditingController controller;
+  final List<TextInputFormatter> inputFormatters;
+  final bool isTapToScanEnabled;
+  final void Function(String)? onScan;
 
-  Future<void> onOpenBarCodeScanner(BuildContext context) async {
-    if (disabled) return;
+  @override
+  State<StatefulWidget> createState() {
+    return BarCodeScannerInputState();
+  }
+}
+
+class BarCodeScannerInputState
+    extends BaseStatefulInputState<BarCodeScannerInput> {
+  late TextEditingController controller;
+  List<TextInputFormatter> inputFormatters = [];
+
+  @override
+  void initState() {
+    controller = TextEditingController(text: widget.value);
+    String? fieldMask = widget.input.fieldMask;
+    inputFormatters.addAll(widget.inputFormatters);
+    if (fieldMask != null) {
+      inputFormatters.add(FilteringTextInputFormatter.allow(RegExp(fieldMask)));
+    }
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant BarCodeScannerInput oldWidget) {
+    if (widget.value == null) {
+      controller = TextEditingController();
+    } else if (widget.value != controller.text) {
+      controller.text = widget.value ?? '';
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  Future<void> onOpenBarCodeScanner(BuildContext context,
+      {bool fromIcon = false}) async {
+    if (widget.disabled) return;
+
+    PermissionStatus status = await Permission.camera.status;
+
+    if (status.isGranted) {
+      _startBarcodeScan(context);
+    } else if (status.isDenied) {
+      PermissionStatus permissionStatus = await Permission.camera.request();
+
+      if (permissionStatus.isGranted) {
+        _startBarcodeScan(
+          context,
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text(
+            'Camera permission is required to scan barcodes.',
+            style: TextStyle(color: Colors.white),
+          )),
+        );
+      }
+    }
+  }
+
+  Future<void> _startBarcodeScan(
+    BuildContext context,
+  ) async {
     try {
       var result = await BarcodeScanner.scan();
       if (result.type == ResultType.Barcode) {
-        onChange(result.rawContent);
+        widget.onScan != null ? widget.onScan!(result.rawContent) : null;
         controller.text = result.rawContent;
       }
     } catch (e) {
+      if (kDebugMode) {
+        print(e.toString());
+      }
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-            backgroundColor: Colors.red,
+        const SnackBar(
             content: Text(
-              'Error scanning barcode: $e',
-              style: const TextStyle(color: Colors.white),
-            )),
+          'Error scanning barcode',
+          style: TextStyle(color: Colors.white),
+        )),
       );
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    controller = TextEditingController(text: value);
     return TextFormField(
-        enabled: !disabled,
-        showCursor: false,
+        enabled: !widget.disabled,
+        cursorColor: widget.decoration.colorScheme.active,
         controller: controller,
-        keyboardType: TextInputType.none,
+        onChanged: (String? value) {
+          widget.onChange(value);
+        },
+        keyboardType: TextInputType.number,
         style: const TextStyle(
           fontSize: 14,
           fontWeight: FontWeight.w500,
@@ -53,23 +125,25 @@ class BarCodeScannerInput
         decoration: InputDecoration(
             border: InputBorder.none,
             suffixIcon: IconButton(
-              color: color,
+              color: widget.color,
               padding: EdgeInsets.zero,
               constraints: iconConstraints,
-              onPressed: disabled
+              onPressed: widget.disabled
                   ? null
                   : () {
                       onOpenBarCodeScanner(context);
                     },
               icon: InputFieldIcon(
-                  backgroundColor: color,
-                  iconColor: color,
+                  backgroundColor: widget.color,
+                  iconColor: widget.color,
                   iconData: Icons.document_scanner_outlined),
             )),
-        onTap: disabled
+        onTap: widget.disabled
             ? null
             : () {
-                onOpenBarCodeScanner(context);
+                if (widget.isTapToScanEnabled) {
+                  onOpenBarCodeScanner(context);
+                }
               });
   }
 }
