d72dd9baaa859dd8b226ccb08bfcd0bdd82de05c
Eric Chingalo <echingalo@gmail.com>
2025-03-26 19:35:03 +0300

    feat(data-set-values): added support for getting values by ou, period and de

--- lib/src/repositories/data/data_value_set.dart
+++ lib/src/repositories/data/data_value_set.dart
@@ -1,8 +1,8 @@
+import 'package:dhis2_flutter_toolkit/dhis2_flutter_toolkit.dart';
 import 'package:dhis2_flutter_toolkit/src/repositories/data/query_mixin/data_value_set_query_mixin.dart';
 import 'package:dhis2_flutter_toolkit/src/repositories/data/upload_mixin/base_aggregate_data_upload_mixin.dart';
 
 import '../../../objectbox.g.dart';
-import '../../models/data/data_value_set.dart';
 import 'base_aggregate.dart';
 import 'download_mixin/base_aggregate_data_download_service_mixin.dart';
 import 'query_mixin/base_aggregate_query_mixin.dart';
@@ -38,6 +38,28 @@ class D2DataValueSetRepository extends D2BaseAggregateRepository<D2DataValueSet>
       D2DataValueSet dataElement) async {
     queryConditions = D2DataValueSet_.dataElement.equals(dataElement.id);
     return query.findAsync();
+  }
+
+  D2DataValueSet? getDataValue({
+    required String orgUnitId,
+    required String periodId,
+    required String dataElementId,
+  }) {
+    D2OrgUnit? orgUnit = D2OrgUnitRepository(db).getByUid(orgUnitId);
+    D2DataElement? dataElement =
+        D2DataElementRepository(db).getByUid(dataElementId);
+
+    if (orgUnit == null || dataElement == null) {
+      return null;
+    }
+
+    return box
+        .query(D2DataValueSet_.organisationUnit.equals(orgUnit.id).and(
+            D2DataValueSet_.period
+                .equals(periodId)
+                .and(D2DataValueSet_.dataElement.equals(dataElement.id))))
+        .build()
+        .findFirst();
   }
 
   @override
