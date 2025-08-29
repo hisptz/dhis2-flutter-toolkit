d72fe842870eb7584ca6e6ec4a81e4a13baf63de
Pipsson <juliusmallya8420@gmail.com>
2025-08-29 16:47:33 +0300

    feat(event): add completeEvent method to update event status to 'COMPLETED'

--- lib/src/models/data/event.dart
+++ lib/src/models/data/event.dart
@@ -290,6 +290,11 @@ class D2Event extends SyncDataSource
     }
   }
 
+  void completeEvent() {
+    status = 'COMPLETED';
+    synced = false;
+  }
+
   @override
   bool delete(D2ObjectBox db) {
     //Deletes event and all associated data values & relationships
