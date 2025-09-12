8abf62193e626697edc0b14501b1cf863446dccf
Eric Chingalo <echingalo@gmail.com>
2025-09-12 23:23:33 +0300

    fix(d2_options): implemented the options download function

--- lib/src/repositories/metadata/download_mixins/option_download_mixin.dart
+++ lib/src/repositories/metadata/download_mixins/option_download_mixin.dart
@@ -4,15 +4,35 @@ import 'package:dhis2_flutter_toolkit/src/repositories/metadata/download_mixins/
 mixin D2OptionDownloadServiceMixin
     on BaseSingleMetaDownloadServiceMixin<D2Option> {
   @override
-  String label = "Option";
+  String label = "Options";
 
   @override
-  String resource = "option";
+  String resource = "options";
 
-  D2OptionDownloadServiceMixin setupDownload(
-    D2ClientService client,
-    {Map<String, String>? queryParams}
-  ) {
+  @override
+  Future<void> download() async {
+    D2SyncStatus status = D2SyncStatus(
+        synced: 0,
+        total: 1,
+        status: D2SyncStatusEnum.initialized,
+        label: label);
+    downloadController.add(status);
+    Map<String, dynamic>? data = await getData<Map<String, dynamic>>();
+    if (data == null) {
+      downloadController.addError("Could not get $label");
+      return;
+    }
+    for (Map<String, dynamic> optionObject in data["options"] ?? []) {
+      D2Option entity = mapper(optionObject);
+      box.put(entity);
+    }
+    status.increment();
+    downloadController.add(status.complete());
+    await downloadController.close();
+  }
+
+  D2OptionDownloadServiceMixin setupDownload(D2ClientService client,
+      {Map<String, String>? queryParams}) {
     setClient(client);
     setFields(["*"]);
     if (queryParams != null) {
