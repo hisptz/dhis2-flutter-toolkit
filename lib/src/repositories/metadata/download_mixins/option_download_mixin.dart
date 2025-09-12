79a4bfbcefa3e5384521b52e20e9cdf33ad028f3
Pipsson <juliusmallya8420@gmail.com>
2025-09-12 14:51:54 +0300

    feat(option_download): add D2OptionDownloadServiceMixin for option downloads

---
+++ lib/src/repositories/metadata/download_mixins/option_download_mixin.dart
@@ -0,0 +1,20 @@
+import 'package:dhis2_flutter_toolkit/dhis2_flutter_toolkit.dart';
+import 'package:dhis2_flutter_toolkit/src/repositories/metadata/download_mixins/base_single_meta_download_mixin.dart';
+
+mixin D2OptionDownloadServiceMixin
+    on BaseSingleMetaDownloadServiceMixin<D2Option> {
+  @override
+  String label = "Option";
+
+  @override
+  String resource = "option";
+
+  D2OptionDownloadServiceMixin setupDownload(
+    D2ClientService client,
+  ) {
+    setClient(client);
+    setFields(["*"]);
+
+    return this;
+  }
+}
