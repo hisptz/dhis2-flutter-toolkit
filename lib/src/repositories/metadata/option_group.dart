34d01c8fa8f16d984660de5c8e678b6ed5f8b1c1
Pipsson <juliusmallya8420@gmail.com>
2025-09-18 12:18:25 +0300

    feat(option_group_repository): integrate download mixins into D2OptionGroupRepository

--- lib/src/repositories/metadata/option_group.dart
+++ lib/src/repositories/metadata/option_group.dart
@@ -1,8 +1,14 @@
+import 'package:dhis2_flutter_toolkit/src/repositories/metadata/download_mixins/base_single_meta_download_mixin.dart';
+import 'package:dhis2_flutter_toolkit/src/repositories/metadata/download_mixins/option_group_download_mixin.dart';
+
 import '../../../objectbox.g.dart';
 import '../../models/metadata/option_group.dart';
 import 'base.dart';
 
-class D2OptionGroupRepository extends BaseMetaRepository<D2OptionGroup> {
+class D2OptionGroupRepository extends BaseMetaRepository<D2OptionGroup>
+    with
+        BaseSingleMetaDownloadServiceMixin<D2OptionGroup>,
+        D2OptionGroupDownloadServiceMixin {
   D2OptionGroupRepository(super.db);
 
   @override
