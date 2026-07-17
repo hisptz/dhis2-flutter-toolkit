1c1bcc828ee0be0b6eb8f1b370990794f12cbe40
tibendadavis <davistibz@gmail.com>
2026-07-17 17:02:50 +0300

    fix: reduce default upload page size to 50 and improve code formatting

--- lib/src/repositories/data/upload_mixin/base_aggregate_data_upload_mixin.dart
+++ lib/src/repositories/data/upload_mixin/base_aggregate_data_upload_mixin.dart
@@ -11,7 +11,7 @@ import '../../../utils/entry.dart';
 mixin BaseAggregateDataUploadServiceMixin<T extends SyncDataSource>
     on D2BaseAggregateRepository<T>, D2BaseAggregateQueryMixin<T> {
   D2ClientService? client;
-  int uploadPageSize = 100;
+  int uploadPageSize = 50;
   abstract String uploadResource;
   abstract String label;
   abstract String uploadDataKey;
@@ -29,8 +29,10 @@ mixin BaseAggregateDataUploadServiceMixin<T extends SyncDataSource>
     return uploadController.stream;
   }
 
-  BaseAggregateDataUploadServiceMixin<T> setupUpload(D2ClientService client,
-      {int? pageSize}) {
+  BaseAggregateDataUploadServiceMixin<T> setupUpload(
+    D2ClientService client, {
+    int? pageSize,
+  }) {
     this.client = client;
     if (pageSize != null) {
       this.uploadPageSize = pageSize;
@@ -44,15 +46,13 @@ mixin BaseAggregateDataUploadServiceMixin<T extends SyncDataSource>
       ..offset = (page * uploadPageSize)
       ..limit = uploadPageSize;
     List<T> entities = await localQuery.findAsync();
-    List<Map<String, dynamic>> entityPayload =
-        await Future.wait(entities.map((entity) => entity.toMap(db: db)));
+    List<Map<String, dynamic>> entityPayload = await Future.wait(
+      entities.map((entity) => entity.toMap(db: db)),
+    );
 
     Map<String, dynamic> payload = {uploadDataKey: entityPayload};
-    Map<String, dynamic> response =
-        await client!.httpPost<Map<String, dynamic>>(
-      uploadURL,
-      payload,
-    );
+    Map<String, dynamic> response = await client!
+        .httpPost<Map<String, dynamic>>(uploadURL, payload);
 
     if (response['httpStatusCode'] == 200) {
       for (T element in entities) {
@@ -76,20 +76,22 @@ mixin BaseAggregateDataUploadServiceMixin<T extends SyncDataSource>
       Query<T> query = getUnSyncedQuery();
       int count = query.count();
       if (count == 0) {
-        uploadController
-            .add(D2SyncStatus(status: D2SyncStatusEnum.complete, label: label));
+        uploadController.add(
+          D2SyncStatus(status: D2SyncStatusEnum.complete, label: label),
+        );
         await uploadController.close();
         return;
       }
       int pages = (count / uploadPageSize).ceil();
       D2SyncStatus status = D2SyncStatus(
-          synced: 0,
-          total: pages,
-          status: D2SyncStatusEnum.initialized,
-          label: label);
+        synced: 0,
+        total: pages,
+        status: D2SyncStatusEnum.initialized,
+        label: label,
+      );
       uploadController.add(status);
       status.updateStatus(D2SyncStatusEnum.syncing);
-      for (int page = 0; page < pages; page++) {
+      for (int page = 0; page <= pages; page++) {
         await uploadPage(page: page, query: query);
         status.increment();
         uploadController.add(status);
